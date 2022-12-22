a814998191dcc315c4b4ae95321f124ae91f3a1a
Aigerim Tuleubayeva <55330888+miregial@users.noreply.github.com>
2022-12-22 05:38:16 +0300

    Решение лаб по потоком и Cuda для второго БДЗ

---
+++ Threads.cpp
@@ -0,0 +1,249 @@
+﻿// Тулеубаева Айгерим М22-524
+// БДЗ 2
+
+#include <iostream>
+#include "bitmap.hpp"
+#include <vector>
+#include <thread>
+#include <cstdio>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+#ifdef __linux__
+
+#include <pthread.h>
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#define fopen_s(pFile, filename, mode) ((*(pFile)) = fopen((filename), (mode))) == NULL
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#endif
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+struct BGR {
+	uint8_t b{ 0 };
+	uint8_t g{ 0 };
+	uint8_t r{ 0 };
+};
+
+std::vector<std::vector<BGR>> make_chunks(std::vector<BGR> data, int chunk_count);
+void count_gt_leq(std::vector<BGR> data, int target, int* leq, int* gt);
+void count_gt_leq_std_thread(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count);
+void count_gt_leq_CreateThread(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count);
+void count_gt_leq_pthread_create(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count);
+
+std::string FIFO_NAME = "undefined";
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 1)
+	{
+		std::cerr << "Передайте аргументом путь к файлу!" << std::endl;
+		exit(1);
+	}
+
+	int target = 128;
+	int leq = 0, gt = 0;
+	BMPMini image;
+	image.read(argv[1]);
+	ImageView imageView = image.get();
+	int size = imageView.width * imageView.height;
+	BGR* pixels = reinterpret_cast<BGR*>(imageView.data);
+	std::vector<BGR> data;
+	for (size_t i = 0; i < size; i++)
+	{
+		data.push_back(pixels[i]);
+	}
+	// Обычная реализация
+	// count_gt_leq(data, target, &leq, &gt);
+
+	// Реализация через std::threads
+	count_gt_leq_std_thread(data, target, &leq, &gt, 4);
+
+	// Реализация через winApi CreateThread
+	// count_gt_leq_CreateThread(data, target, &leq, &gt, 4);
+
+	// Реализация через Linux pthread_create
+	// count_gt_leq_pthread_create(data, target, &leq, &gt, 4);
+
+	std::cout << "Length: " << data.size() << std::endl;
+	std::cout << "Target: " << target << std::endl;
+	std::cout << "Less or equal than target: " << leq << std::endl;
+	std::cout << "Great than target: " << gt << std::endl;
+	std::cout << "Sum of leq and gt: " << gt + leq << std::endl;
+}
+
+struct params
+{
+	std::vector<BGR> data;
+	int target;
+	int leq;
+	int gt;
+};
+
+struct params_pthread
+{
+	std::vector<BGR> data;
+	int target;
+	int* leq;
+	int* gt;
+};
+
+/// <summary>
+/// Реализация задания без парралельности
+/// </summary>
+/// <param name="data">Массив данных</param>
+/// <param name="target">Значения относительно которого мы проверяем</param>
+/// <param name="leq">Значения по ссылке, возвращаемый параметр </param>
+/// <param name="gt"></param>
+void count_gt_leq(std::vector<BGR> data, int target, int* leq, int* gt)
+{
+	for (auto value : data)
+	{
+		if (value.r > target)
+			(*gt)++;
+		else
+			(*leq)++;
+	}
+}
+
+#ifdef __linux__
+void* count_gt_leq_thread_safe(void* args)
+{
+	params_pthread* param = (params_pthread*)args;
+	for (auto value : param->data)
+	{
+		// Использовавания мютексов по заданию, для большей произоводительности их не нужно использовать, а передавать разные ссылки
+		pthread_mutex_lock(&mutex);
+		if (value.r > param->target)
+			(*(param->gt))++;
+		else
+			(*(param->leq))++;
+		pthread_mutex_unlock(&mutex);
+	}
+}
+#endif
+
+/// <summary>
+/// Многопоточная реализация с использоваванием std::thread
+/// </summary>
+/// <param name="thread_count">Кол-во потоков</param>
+void count_gt_leq_std_thread(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count)
+{
+	std::vector<std::thread> thread_list;
+	auto chunks = make_chunks(data, thread_count);
+	std::vector<std::pair<int, int>> params(thread_count);
+	for (int i = 0; i < thread_count; i++)
+	{
+		thread_list.push_back(std::thread(count_gt_leq, chunks[i], target, &(params[i].first), &(params[i].second)));
+	}
+
+	for (int i = 0; i < thread_list.size(); i++)
+	{
+		thread_list[i].join();
+	}
+
+	for (auto param : params)
+	{
+		(*leq) += param.first;
+		(*gt) += param.second;
+	}
+}
+
+#ifdef _WIN32
+DWORD __stdcall count_gt_leq_wrapper(void* args)
+{
+	params* param = (params*)args;
+	count_gt_leq(param->data, param->target, &(param->leq), &(param->gt));
+	return 0;
+}
+#endif
+
+void count_gt_leq_CreateThread(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count)
+{
+	auto chunks = make_chunks(data, thread_count);
+	std::vector<params> params(thread_count);
+#ifdef _WIN32
+	std::vector<HANDLE> threads_handles;
+
+	for (int i = 0; i < thread_count; i++)
+	{
+		params[i].data = chunks[i];
+		params[i].leq = 0;
+		params[i].gt = 0;
+		params[i].target = target;
+
+		auto handle = CreateThread(NULL, 0, &count_gt_leq_wrapper, (void*)&params[i], 0, NULL);
+
+		if (handle == NULL)
+		{
+			throw std::exception("Не удалось создать поток");
+		}
+
+		threads_handles.push_back(handle);
+	}
+
+	WaitForMultipleObjects(threads_handles.size(), &threads_handles[0], true, INFINITE);
+#else
+	std::cerr << "OS not supported" << std::endl;
+#endif
+	for (int i = 0; i < params.size(); i++)
+	{
+		(*leq) += params[i].leq;
+		(*gt) += params[i].gt;
+	}
+}
+
+void count_gt_leq_pthread_create(std::vector<BGR> data, int target, int* leq, int* gt, int thread_count)
+{
+#ifdef __linux__
+	auto chunks = make_chunks(data, thread_count);
+	std::vector<pthread_t> threads_id(thread_count);
+	std::vector<params_pthread> params(thread_count);
+
+	for (int i = 0; i < thread_count; i++)
+	{
+		params[i].data = chunks[i];
+		params[i].leq = leq;
+		params[i].gt = gt;
+		params[i].target = target;
+
+		int res = pthread_create(&threads_id[i], NULL, &count_gt_leq_thread_safe, (void*)&params[i]);
+
+		if (res != 0)
+		{
+			throw std::exception();
+		}
+	}
+
+	for (int i = 0; i < thread_count; i++)
+	{
+		pthread_join(threads_id[i], NULL);
+	}
+#else
+	std::cerr << "OS not supported" << std::endl;
+#endif
+}
+
+std::vector<std::vector<BGR>> make_chunks(std::vector<BGR> data, int chunk_count)
+{
+	std::vector<std::vector<BGR>> datas(chunk_count);
+
+	for (int i = 0; i < data.size(); i++)
+	{
+		int chunk_index = i % chunk_count;
+		datas[chunk_index].push_back(data[i]);
+	}
+	return datas;
+}
---
+++ cuda.cu
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include "bitmap.hpp"
+#include <algorithm>
+
+struct BGR {
+    uint8_t b { 0 };
+    uint8_t g { 0 };
+    uint8_t r { 0 };
+};
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
+
+template<typename T>
+void check(T err, const char* const func, const char* const file, const int line) {
+    if (err != cudaSuccess) {
+        std::cerr << "Err: CUDA error at: " << file << ":" << line << std::endl;
+        std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+        exit(1);
+    }
+}
+
+#define min(a, b) (a > b)? b: a
+#define max(a, b) (a > b)? a: b 
+
+__global__ 
+void kernel(BGR* data, int size, int *res, BGR* grayscale)
+{
+    // считает кол-во красных пикселей
+	int index = blockDim.x * blockIdx.x + threadIdx.x;
+    if (index >= size) return;
+
+	BGR pixel = data[index];
+    BGR grayscalePixel;
+
+    // приводим картинку к полутону 
+    uint8_t grayPixel = (uint8_t)min(max(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b, 0.0), 255.0);
+    grayscalePixel.r = grayPixel;
+    grayscalePixel.g = grayPixel;
+    grayscalePixel.b = grayPixel;
+    
+    if (pixel.r > 128) res[index]++;
+    grayscale[index] = grayscalePixel;
+}
+
+int main()
+{
+    BMPMini bmp;
+    bmp.read("Lena.bmp");
+    auto img = bmp.get();
+    printf("Readed an image\n");
+    cudaError_t cudaStatus;
+    BGR* pixels = reinterpret_cast<BGR*>(img.data);
+        
+    BGR* d_data = 0;
+    BGR* d_greyscale = 0;
+    BGR* h_greyscale = 0;
+    int* d_res = 0;
+    int* h_res = 0;
+    int res = 0;
+    int size = img.height * img.width;
+    printf("size of image: h=%d, w=%d, channels=%d", img.height, img.width, img.channels);
+    const int block_size = 1024;
+    const int grids = size / block_size + 1;
+
+    h_res = new int[block_size];
+    h_greyscale = new BGR[size];
+
+    checkCudaErrors(cudaMalloc((void**)&d_data, size * sizeof(BGR)));
+
+    checkCudaErrors(cudaMalloc((void**)&d_res, block_size * sizeof(int)));
+    checkCudaErrors(cudaMalloc((void**)&d_greyscale, size * sizeof(BGR)));
+
+    checkCudaErrors(cudaMemcpy(d_data, pixels, size * sizeof(BGR), cudaMemcpyHostToDevice));
+
+    printf("Allocated 4 step\n");
+    kernel<<<grids, block_size>>>(d_data, size, d_res, d_greyscale);
+
+    printf("Allocated 5 step\n");
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
+
+    printf("Allocated 6 step");
+
+    checkCudaErrors(cudaMemcpy(h_res, d_res, block_size * sizeof(int), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(h_greyscale, d_greyscale, size * sizeof(BGR), cudaMemcpyDeviceToHost));
+    printf("Allocated 7 step");
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
+    for (size_t i = 0; i < block_size; i++)
+    {
+        res += h_res[i];
+    }
+    printf("Allocated 8 step");
+    printf("Count of reds that > 128 = %d", res);
+    
+    int j = 0;
+    for(int i = 0; i < size; i++) {
+        img.data[j++] = h_greyscale[i].b;
+        img.data[j++] = h_greyscale[i].g;
+        img.data[j++] = h_greyscale[i].r;
+    }
+    bmp.write(img, "lena_gray.bmp");
+}
